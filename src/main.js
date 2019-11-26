7bfa0ed20ec23e127fa22b6eec74b77dd42a3f84
NeverYu <never_yu@foxmail.com>
2019-11-26 19:14:32 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,6 +5,19 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online !!!
+ */
+import { mockXHR } from '../mock'
+if(process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
+
 // 按需引入部分组件
 import { Button, DatetimePicker, Popup } from 'mint-ui'
 Vue.component(Button.name, Button)
