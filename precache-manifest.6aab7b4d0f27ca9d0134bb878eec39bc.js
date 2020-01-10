bcc59c95179901221438cefb1cd8238fadb869d8
Travis CI User <travis@example.org>
2020-01-10 03:20:41 +0000

    deploy

---
+++ precache-manifest.6aab7b4d0f27ca9d0134bb878eec39bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21d5f03d8bd08f48e77b40aa2ee00f1",
+    "url": "/map-chart/css/reset.css"
+  },
+  {
+    "revision": "74b0cb0bdf6203ebfe641b2f0aed9eda",
+    "url": "/map-chart/img/page-qr.png"
+  },
+  {
+    "revision": "55b38e01535c60bf490fc578a28322a2",
+    "url": "/map-chart/index.html"
+  },
+  {
+    "revision": "1a87d2aedf0bf8894d01a7b59b863232",
+    "url": "/map-chart/index.html.bak"
+  },
+  {
+    "revision": "b121962120a53e51335c2aa81e7fceb1",
+    "url": "/map-chart/index.template.html"
+  },
+  {
+    "revision": "ddabd5646177596b76ecbf28d06eb562",
+    "url": "/map-chart/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/map-chart/robots.txt"
+  },
+  {
+    "revision": "6adba77d3873ceffe676",
+    "url": "/map-chart/static/css/app.aecf0c8f.css"
+  },
+  {
+    "revision": "c02f1c2d25e1e0326731",
+    "url": "/map-chart/static/css/chunk-4a54d868.313a242f.css"
+  },
+  {
+    "revision": "a1ece365efade686c0b8",
+    "url": "/map-chart/static/css/chunk-vendors.d508229c.css"
+  },
+  {
+    "revision": "5ad8d7133cc02599dfdaedfb0d560b31",
+    "url": "/map-chart/static/img/map-label.5ad8d713.png"
+  },
+  {
+    "url": "/map-chart/static/js/about.0ca57987d62da60a9b51.0-3-0.js"
+  },
+  {
+    "url": "/map-chart/static/js/app.0ca57987d62da60a9b51.0-3-0.js"
+  },
+  {
+    "url": "/map-chart/static/js/chunk-4a54d868.0ca57987d62da60a9b51.0-3-0.js"
+  },
+  {
+    "url": "/map-chart/static/js/chunk-vendors.0ca57987d62da60a9b51.0-3-0.js"
+  }
+]);
